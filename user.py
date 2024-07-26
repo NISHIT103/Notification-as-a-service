796d3ff1b1cd8d036a9d487913044e11c320305a
NISHIT103 <32533436+NISHIT103@users.noreply.github.com>
2024-07-26 15:00:06 +0530

    Create user.py

---
+++ user.py
@@ -0,0 +1,8 @@
+import requests
+from authentication import auth
+
+class user:
+    def __init__():
+        username = None
+        password = auth.generatehashedpasword("nisht_password")
+    # def user_details:
